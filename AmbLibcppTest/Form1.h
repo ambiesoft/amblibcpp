97130ddb6015e05a1f3a6c0c9b0dd17f2660bf85
ambiesoft <ambiesoft.trueff@gmail.com>
2024-11-21 20:13:28 +0900

    nousascii

--- AmbLibcppTest/Form1.h
+++ AmbLibcppTest/Form1.h
@@ -12,13 +12,6 @@ namespace AmbLibcppTest {
 	using namespace Ambiesoft;
 
 	/// <summary>
-	/// Form1 の概要
-	///
-	/// 警告: このクラスの名前を変更する場合、このクラスが依存するすべての .resx ファイルに関連付けられた
-	///          マネージ リソース コンパイラ ツールに対して 'Resource File Name' プロパティを
-	///          変更する必要があります。この変更を行わないと、
-	///          デザイナと、このフォームに関連付けられたローカライズ済みリソースとが、
-	///          正しく相互に利用できなくなります。
 	/// </summary>
 	public ref class Form1 : public System::Windows::Forms::Form
 	{
@@ -31,7 +24,6 @@ namespace AmbLibcppTest {
 
 	protected:
 		/// <summary>
-		/// 使用中のリソースをすべてクリーンアップします。
 		/// </summary>
 		~Form1()
 		{
@@ -59,14 +51,11 @@ namespace AmbLibcppTest {
 
 	private:
 		/// <summary>
-		/// 必要なデザイナ変数です。
 		/// </summary>
 		System::ComponentModel::Container ^components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
-		/// デザイナ サポートに必要なメソッドです。このメソッドの内容を
-		/// コード エディタで変更しないでください。
 		/// </summary>
 		void InitializeComponent(void)
 		{
